bad0f133b287a08c63d3ecbdafb48fbaec31ac32
Francis Espiritu <jfrancis.espiritu@gmail.com>
2025-04-18 17:55:54 +0800

    more edit on the bg

--- lib/utils/background.dart
+++ lib/utils/background.dart
@@ -12,11 +12,11 @@ class BackgroundPage extends StatelessWidget {
     return Stack(
       children: [
         Positioned(
-          top: MediaQuery.of(context).size.height * -0.22,
-          left: MediaQuery.of(context).size.width * 0.6,
+          top: MediaQuery.of(context).size.height * -0.20,
+          left: MediaQuery.of(context).size.width * 0.68,
           child: Container(
             height: screenHeight * 0.9, // Adjust size
-            width: screenWidth * 0.8, // Adjust size
+            width: screenWidth * 1, // Adjust size
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               color: Theme.of(context).colorScheme.secondary,
@@ -30,23 +30,23 @@ class BackgroundPage extends StatelessWidget {
               0.1, // LOWER MEANS IT GOES UP, HIGHER MEANS IT GOES DOWN
           left:
               MediaQuery.of(context).size.width *
-              -0.3, //LOWER MEANS IT GOES TO RIGHT, HIGHER MEANS IT GOES TO LEFT
+              -0.65, //LOWER MEANS IT GOES TO RIGHT, HIGHER MEANS IT GOES TO LEFT
           child: Container(
             height: screenHeight * 0.9, // Adjust size
-            width: screenWidth * 0.6, // Adjust size
+            width: screenWidth * 1, // Adjust size
             decoration: BoxDecoration(
               shape: BoxShape.circle,
-              color: Theme.of(context).colorScheme.primary,
+              color: Color.fromARGB(255, 94, 201, 176),
             ),
           ),
         ),
 
         Positioned(
-          top: MediaQuery.of(context).size.height * 0.55,
-          left: MediaQuery.of(context).size.width * 0.75,
+          top: MediaQuery.of(context).size.height * 0.60,
+          left: MediaQuery.of(context).size.width * 0.70,
           child: Container(
             height: screenHeight * 0.4, // Adjust size
-            width: screenWidth * 0.4, // Adjust size
+            width: screenWidth * 0.8, // Adjust size
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               color: Theme.of(context).colorScheme.primary,
@@ -55,7 +55,7 @@ class BackgroundPage extends StatelessWidget {
         ),
 
         BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 80.0, sigmaY: 125.0),
+          filter: ImageFilter.blur(sigmaX: 100.0, sigmaY: 125.0),
           child: Container(color: Colors.transparent),
         ),
       ],
